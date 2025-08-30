46665d8960e920036e878cbda9610dc62cdebd7b
WangilaPrish <nabwonyaprish@gmail.com>
2025-08-30 14:47:00 +0300

    Add categories view and URL mapping for improved navigation

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('about/', views.about, name='about'),
+    path('categories/', views.categories, name='categories'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -14,3 +14,6 @@ def about(request):
 def product_list(request):
 	products = Product.objects.all()
 	return render(request, 'shop/product_list.html', {'products': products})
+
+def categories(request):
+	return render(request, 'categories.html')
